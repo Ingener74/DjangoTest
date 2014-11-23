e1e284ebfc2a5263fdd4e2355eca084733cb6649
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-11-23 20:36:14 +0300

    last

--- untitled/urls.py
+++ untitled/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'untitled.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
